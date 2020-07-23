bd660077bd58f671152b65379159336842437fc4
jiwen xin <kevinxin@scripps.edu>
2020-07-23 15:46:31 -0700

    add _id

--- parser.py
+++ parser.py
@@ -4,8 +4,9 @@ import obonet
 def load_data(data_folder):
     url = "https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo"
     graph = obonet.read_obo(url)
-    for item in graph.nodes():
+    for item in graph.nodes()
         rec = graph.nodes[item]
+        rec["_id"] = item
         if rec.get("is_a"):
             rec["parents"] = rec.pop("is_a")
         if rec.get("xrefs"):
