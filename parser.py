2994c3d90b71dfba2568737f186b02d049715707
jiwen xin <kevinxin@scripps.edu>
2020-08-20 09:31:10 -0700

    update synonyms

--- parser.py
+++ parser.py
@@ -1,6 +1,37 @@
 import networkx as nx
 import obonet
 from collections import defaultdict
+import re
+
+def get_synonyms(data):
+    """Format synonyms as dicionary
+    exact and related synonyms are the keys, and their values are in lists
+    """
+    if 'synonym' in data:
+        syn_dict = {}
+        exact = []
+        related = []
+        broad = []
+        for syn in data['synonym']:
+            if 'EXACT' in syn: 
+                match = re.findall(r'\"(.+?)\"', syn)
+                exact = exact + match
+            elif 'RELATED' in syn: 
+                match = re.findall(r'\"(.+?)\"', syn)
+                related = related + match
+            elif 'BROAD' in syn:
+                match = re.findall(r'\"(.+?)\"', syn)
+                broad = broad + match
+        synonyms = {}
+        if len(exact) > 0:
+            synonyms["exact"] = exact
+        if len(related) > 0:
+            synonyms["related"] = related
+        if len(broad) > 0:
+            synonyms["broad"] = broad
+        return synonyms
+    else:
+        return {}
 
 def load_data(data_folder):
     url = "https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.obo"
@@ -30,6 +61,7 @@ def load_data(data_folder):
         rec["children"] = [child for child in graph.predecessors(item) if child.startswith("HP:")]
         rec["ancestors"] = [ancestor for ancestor in nx.descendants(graph, item) if ancestor.startswith("HP:")]
         rec["descendants"] = [descendant for descendant in nx.ancestors(graph,item) if descendant.startswith("HP:")]
+        rec["synonym"] = get_synonyms(rec)
         if rec.get("created_by"):
             rec.pop("created_by")
         if rec.get("creation_date"):
